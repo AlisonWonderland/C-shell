66b0f040295151460bf112436d2cd7db6b45dbeb
AlisonWonderland <dimitridawn@gmail.com>
2019-03-28 01:51:11 +0000

    Convert file type to C

--- shell.c
+++ shell.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
+/*
 int main() {
-    pid_t child_pid = fork();
+    pid_t child_pid;
+    pid_t wait_result;
+    int stat_loc;
+
+    child_pid = fork();
         
     // The child process
     if (child_pid == 0) {
         printf("### Child ###\nCurrent PID: %d and Child PID: %d\n",
                getpid(), child_pid);
-    } 
-    
-    else {
         sleep(1); // Sleep for one second
+    } else {
+        wait_result = waitpid(child_pid, &stat_loc, WUNTRACED);
         printf("### Parent ###\nCurrent PID: %d and Child PID: %d\n",
                getpid(), child_pid);
     }
- 
+    
+    return 0;
+}
+*/
+int main() {
+    char *argv[] = {"ls", NULL};
+    execvp(argv[0], argv);
+
     return 0;
 }
