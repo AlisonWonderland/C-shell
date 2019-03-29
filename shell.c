4f9f57f5cc3e5905c49ed835a54bc4bc39649fab
AlisonWonderland <dimitridawn@gmail.com>
2019-03-29 01:49:01 +0000

    First version of shell

--- shell.c
+++ shell.c
@@ -1,11 +1,52 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+
+char **get_input(char *input) {
+    char **command = malloc(8 * sizeof(char *));
+    char *separator = " ";
+    char *parsed;
+    int index = 0;
+
+    parsed = strtok(input, separator);
+    while (parsed != NULL) {
+        command[index] = parsed;
+        index++;
+
+        parsed = strtok(NULL, separator);
+    }
+
+    command[index] = NULL;
+    return command;
+}
+
 int main() {
-    char *argv[] = {"ls", "-l", "-h", "-a", NULL};
-    execvp(argv[0], argv);
+    char **command;
+    char *input;
+    pid_t child_pid;
+    int stat_loc;
+
+    while (1) {
+        input = readline("mShell> ");
+        command = get_input(input);
+
+        child_pid = fork(); //new child is made to ensure that the main process/shell doesn't close
+        if (child_pid == 0) {
+            /* Never returns if the call is successful */
+            execvp(command[0], command);
+            printf("This won't be printed if execvp is successul\n");
+        } else {
+            waitpid(child_pid, &stat_loc, WUNTRACED);
+        }
+
+        free(input);
+        free(command);
+    }
 
     return 0;
 }
